e5f5862540d9c9b645bf2dd3e6bb6581267e1cce
MR <rutilio_3190@hotmail.com>
2019-01-08 19:46:17 -0600

    1

---
+++ Main/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+public class Main {
+public static void main(String[] args) {
+//este es un comentario
+        /*
+        este es un comentario
+        de multiples lineas
+         */
+        System.out.println("Hola Mundo");
+        System.out.println("Adios");
+    }
+}
+
