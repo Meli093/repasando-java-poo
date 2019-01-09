4afb382d049bae14990e71817ab7c30bfe609788
MR <rutilio_3190@hotmail.com>
2019-01-08 20:38:27 -0600

    2

---
+++ Main/src/Concatenacion/main.java
@@ -0,0 +1,18 @@
+package Concatenacion;
+
+/**
+ *
+ * @author MR
+ */
+public class main {
+public static void main(String[] args) {
+        String usuario = "Juan";
+        String saludar = "Hola";
+        System.out.println(saludar + " " + usuario);
+
+        //Saludos, mi nombre es Juan
+        System.out.println("Saludos, mi nombre es" + " " + usuario);
+
+        System.out.println("saludar = " + saludar);    
+}
+}
---
+++ Main/src/variable/Main.java
@@ -0,0 +1,18 @@
+package variable;
+public class Main {
+public static void main(String[] args) {
+String saludos = "Hola Mundo, adios.";
+        System.out.println(saludos);
+        System.out.println(saludos);
+        System.out.println(saludos);
+
+        String  s = "despedirse";
+        System.out.println();
+        System.out.println(s);
+
+    }
+}
+
+    
+
+
